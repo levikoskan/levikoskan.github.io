90e590b2ae54d76f478c91753b44851b13040ea9
levi koskan <levikoskan@gmail.com>
2016-02-26 08:11:51 -0600

    removed giphy api

--- script.js
+++ script.js
@@ -34,37 +34,6 @@ $(function(){
     });
   // }
 
-  var giphyApiRoot = 'https://api.giphy.com/';
-  var apiKey = 'dc6zaTOxFJmzC'
-
-
-    $('.search').on('submit', function(){
-      $('#results').empty();
-      var searchTerm = $('#q').val();
-      console.log('The form was submitted');
-      console.log('The value submitted was: ', searchTerm);
-
-      $.ajax({
-        method: "GET",
-
-        url: giphyApiRoot + 'v1/gifs/random',
-        // whatever data we put in here will be added on to the URL
-        data: {
-          api_key: apiKey,
-          tag: searchTerm
-        }
-      })
-
-      .done(function(giphy){
-        console.log(giphy);
-
-        $('#results').append('<img src="'+giphy.data.image_original_url+'">');
-
-      });
-
-
-      return false;
-  });
 
 
 
